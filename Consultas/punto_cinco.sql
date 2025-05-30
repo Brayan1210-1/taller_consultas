94b17d4f9b0e995b6769f8bfbf8affb547f9e16b
Brayan1210-1 <alexisquebrada11@gmail.com>
2025-05-29 22:11:53 -0500

    Delete Consultas directory

--- Consultas/punto_cinco.sql
+++
@@ -1,9 +0,0 @@
--- 5. Clientes sin compras desde su alta
-
-SELECT `c`.`nombre`,
-`c`.`apellido`,
-`v`.`cliente_id` AS `compra`
-FROM `tienda`.`cliente` `c`
-LEFT JOIN `tienda`.`venta` `v` ON  `c`.`id` = `v`.`cliente_id` WHERE `cliente_id` IS NULL
-
--- La consulta da como resultado los nombres y apellidos de los clientes que no han hecho compras
--- Consultas/punto_seis.sql
+++
@@ -1,9 +0,0 @@
--- 6. Ticket promedio por venta
-
-SELECT 
-AVG(`cantidad`*`precio_unitario`) AS `promedio`,
-COUNT(num_venta) AS `total_venta`
-FROM `tienda`.`venta` `v`
-INNER JOIN `tienda`.`detalle_venta` `dv` ON `v`.`detalle_venta_id` = `dv`.`id`
-
--- La consulta muestra el promedio y número de todas las ventas
